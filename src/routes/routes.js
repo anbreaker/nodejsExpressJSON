7fcff91a69542e4e9f996a532925216eb0528999
anbreaker <antunez19@gmail.com>
2020-08-30 21:58:28 +0200

    recibiendo datos en html...

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,36 @@
-const {Router} = require('express');
+const {Router, request} = require('express');
 const router = Router();
 
+const books = [];
+
 router.get('/', (request, response) => {
-  response.send('Hello world');
+  response.render('index.ejs', {
+    books,
+  });
+});
+
+router.get('/new-entry', (request, response) => {
+  response.render('new-entry');
+});
+
+router.post('/new-entry', (request, response) => {
+  const {title, author, image, description} = request.body;
+
+  if (!title || !author || !image || !description) {
+    response.status(400).send('Entries must have all field');
+    return;
+  }
+
+  let newBook = {
+    title,
+    author,
+    image,
+    description,
+  };
+
+  books.push(newBook);
+  response.send('recived');
+  console.log('ver', books);
 });
 
 module.exports = router;
